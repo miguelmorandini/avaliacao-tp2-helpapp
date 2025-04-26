c61d49a41ee3970af7c41cb44060579624ab34b0
miguelmorandini <miguelmorandini2005@gmail.com>
2025-04-26 17:17:37 -0300

    implementação dos repositórios Product e Category, de EntitiesConfiguration, de injeção de dependência, e de conexao com sql server

--- HelpApp/HelpApp.Infra.Data/Repositories/CategoryRepository.cs
+++ HelpApp/HelpApp.Infra.Data/Repositories/CategoryRepository.cs
@@ -1,12 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HelpApp.Domain.Entities;
+using HelpApp.Domain.Interfaces;
+using HelpApp.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace HelpApp.Infra.Data.Repositories
 {
-    public class CategoryRepository
+    public class CategoryRepository : ICategoryRepository
     {
+        #region Fields
+        private readonly ApplicationDbContext _dbContext;
+        #endregion
+
+        #region Constructor
+        public CategoryRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        #endregion
+
+        #region Methods
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            var categories = await _dbContext.Categories.AsNoTracking().OrderBy(category => category.Name).ToListAsync();
+            return categories;
+        }
+
+        public async Task<Category> GetById(int? id)
+        {
+            ValidateId(id);
+            var category = await _dbContext.Categories.FindAsync(id);
+            return category;
+        }
+
+        public async Task<Category> Create(Category category)
+        {
+            ValidateCategory(category);
+            _dbContext.Categories.Add(category);
+            await _dbContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> Update(Category category)
+        {
+            ValidateCategory(category);
+            _dbContext.Categories.Update(category);
+            await _dbContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> Remove(Category category)
+        {
+            ValidateCategory(category);
+            _dbContext.Categories.Remove(category);
+            await _dbContext.SaveChangesAsync();
+            return category;
+        }
+        #endregion
+
+        #region Validation
+        private void ValidateCategory(Category? category)
+        {
+            if (category == null)
+                throw new ArgumentNullException(nameof(category), "Category is required.");
+        }
+        private void ValidateId(int? id)
+        {
+            if (!id.HasValue || id <= 0)
+                throw new ArgumentException("Id must be positive.", nameof(id));
+        }
+        #endregion
     }
 }
